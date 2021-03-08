23f3f669544a2b0b28377fd98c409eecabe9c86d
sergeybelyakov <belyakovsergeyrus@yandex.ru>
2021-03-08 20:01:00 +0300

    first upload

---
+++ app/src/main/java/com/sv/newsapp/activities/MainActivity.java
@@ -0,0 +1,260 @@
+package com.sv.newsapp.activities;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.sv.newsapp.NewsDetailActivity;
+import com.sv.newsapp.R;
+import com.sv.newsapp.adapters.Adapter;
+import com.sv.newsapp.models.Article;
+import com.sv.newsapp.models.News;
+import com.sv.newsapp.models.api.ApiClient;
+import com.sv.newsapp.models.api.ApiInterface;
+import com.sv.newsapp.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private final String API_KEY = "041507c2165e4544b6fbdb2eb40dfa55";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private ImageView btnSwitch;
+    private String TAG = MainActivity.class.getSimpleName();
+    private TextView topHeadLine;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RelativeLayout errorLayout;
+    private ImageView errorImage;
+    private TextView errorTitle, errorMessage;
+    private Button btnRetry;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+
+        topHeadLine = findViewById(R.id.top_headline);
+        recyclerView = findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+
+        onLoadingSwipeRefresh("");
+
+        errorLayout = findViewById(R.id.errorLayout);
+        errorImage = findViewById(R.id.errorImage);
+        errorTitle = findViewById(R.id.errorTitle);
+        errorMessage = findViewById(R.id.errorMessage);
+        btnRetry = findViewById(R.id.btnRetry);
+
+    }
+
+    public void LoadJson(final String keyword) {
+
+        errorLayout.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(true);
+
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+
+        String country = Utils.getCountry();
+        String language = Utils.getLanguage();
+
+        Call<News> call;
+        if (keyword.length() > 0) {
+            call = apiInterface.getNewsSearch(keyword, language, "publishedAt", API_KEY);
+        } else {
+            call = apiInterface.getNews(country, API_KEY);
+        }
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticles() != null) {
+                    if (!articles.isEmpty()) {
+                        articles.clear();
+                    }
+
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(articles, MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                    initListener();
+
+                    topHeadLine.setVisibility(View.VISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+
+                } else {
+
+                    topHeadLine.setVisibility(View.INVISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+                    String errorCode;
+                    switch (response.code()) {
+                        case 404:
+                            errorCode = "404 not Found";
+                            break;
+                        case 500:
+                            errorCode = "500 server broken";
+                            break;
+                        default:
+                            errorCode = "unknown error";
+                            break;
+                    }
+
+                    showErrorMessage(
+                            R.drawable.no_result,
+                            "No Result",
+                            "Please, try again\n" +
+                                    errorCode);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+                topHeadLine.setVisibility(View.INVISIBLE);
+                swipeRefreshLayout.setRefreshing(false);
+                showErrorMessage(
+                        R.drawable.no_result,
+                        "Oops...",
+                        "Network failure, please try again\n" +
+                                t.toString());
+            }
+        });
+    }
+
+    private void initListener() {
+        adapter.setOnItemClickListener((view, position) -> {
+//            ImageView imageView = view.findViewById(R.id.img);
+            Intent intent = new Intent(MainActivity.this, NewsDetailActivity.class);
+
+            Article article = articles.get(position);
+            intent.putExtra("url", article.getUrl());
+            intent.putExtra("title", article.getTitle());
+            intent.putExtra("img", article.getUrlToImage());
+            intent.putExtra("date", article.getPublishedAt());
+            intent.putExtra("source", article.getSource().getName());
+            intent.putExtra("author", article.getAuthor());
+
+//            Pair<View, String> pair = Pair.create((View)imageView,
+//                    ViewCompat.getTransitionName(imageView));
+//            ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(
+//                    this, pair);
+            startActivity(intent);
+//            startActivity(intent, activityOptionsCompat.toBundle());
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setQueryHint("Поиск последних новостей ...");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (query.length() > 2) {
+                    onLoadingSwipeRefresh(query);
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchMenuItem.getIcon().setVisible(false, false);
+
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_weather) {
+//            Intent intent = new Intent(Intent.ACTION_VIEW);
+//            intent.setData(Uri.parse(mUrl));
+//            startActivity(intent);
+//            return true;
+        } else if (id == R.id.action_settings) {
+//            try {
+//                Intent intent = new Intent(Intent.ACTION_SEND);
+//                intent.setType("text/plan");
+//                intent.putExtra(Intent.EXTRA_SUBJECT, mSource);
+//                String body = mTitle + "\n" + mUrl + "\n" + "Share from the News App" + "\n";
+//                intent.putExtra(Intent.EXTRA_TEXT, body);
+//                startActivity(Intent.createChooser(intent, "Share with :"));
+//            } catch (Exception e) {
+//                Toast.makeText(this, "Hmm.. Sorry, \nCannot be share", Toast.LENGTH_SHORT).show();
+//            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRefresh() {
+        LoadJson("");
+    }
+
+    private void onLoadingSwipeRefresh(final String keyword) {
+        swipeRefreshLayout.post(
+                () -> LoadJson(keyword)
+        );
+    }
+
+    private void showErrorMessage(int imageView, String title, String message ) {
+        if (errorLayout.getVisibility() == View.GONE) {
+            errorLayout.setVisibility(View.VISIBLE);
+        }
+
+        errorImage.setImageResource(imageView);
+        errorTitle.setText(title);
+        errorMessage.setText(message);
+
+        btnRetry.setOnClickListener(v -> onLoadingSwipeRefresh(""));
+    }
+}
---
+++ app/src/main/java/com/sv/newsapp/models/api/ApiClient.kt
@@ -0,0 +1,73 @@
+package com.sv.newsapp.models.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+object ApiClient {
+    const val BASE_URL = "https://newsapi.org/v2/"
+    var retrofit: Retrofit? = null
+
+    @JvmStatic
+    val apiClient: Retrofit?
+        get() {
+            if (retrofit == null) {
+                retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+                    .client(unsafeOkHttpClient.build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
+
+    // Create a trust manager that does not validate certificate chains
+    val unsafeOkHttpClient: OkHttpClient
+
+    // Install the all-trusting trust manager
+
+    // Create an ssl socket factory with our all-trusting manager
+    .Builder
+        get() = try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts = arrayOf<TrustManager>(
+                object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(
+                        chain: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(
+                        chain: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate> {
+                        return arrayOf()
+                    }
+                }
+            )
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.socketFactory
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            builder.hostnameVerifier { hostname, session -> true }
+            builder
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+}
