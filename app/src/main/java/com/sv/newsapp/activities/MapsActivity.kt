11e64dd25f5345f18768a6dc8e6dfaf08a19f76f
sergeybelyakov <belyakovsergeyrus@yandex.ru>
2021-05-26 15:17:20 +0300

    UPD: actual version by 26.05.2021 (2)

--- app/src/main/java/com/sv/newsapp/activities/MapsActivity.kt
+++ app/src/main/java/com/sv/newsapp/activities/MapsActivity.kt
@@ -1,99 +1,85 @@
-package com.sv.newsapp.activities;
+package com.sv.newsapp.activities
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.sv.newsapp.R
+import com.sv.newsapp.data.WeatherData
+import com.sv.newsapp.models.api.Api
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.FragmentActivity;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.sv.newsapp.R;
-import com.sv.newsapp.data.WeatherData;
-import com.sv.newsapp.models.api.Api;
-
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
-
-    private GoogleMap mMap;
-
-    private final Api mApi = Api.Instance.getApi();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+class MapsActivity : FragmentActivity(), OnMapReadyCallback, View.OnClickListener {
+    private var mMap: GoogleMap? = null
+    private val mApi = Api.Instance.getApi()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        findViewById(R.id.moscow_button).setOnClickListener(this);
-        findViewById(R.id.spb_button).setOnClickListener(this);
-        findViewById(R.id.news_button).setOnClickListener(this);
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+        findViewById<View>(R.id.moscow_button).setOnClickListener(this)
+        findViewById<View>(R.id.spb_button).setOnClickListener(this)
+        findViewById<View>(R.id.news_button).setOnClickListener(this)
     }
 
     @SuppressLint("CheckResult")
-    private void receiveWeather(final String city) {
-        mApi.getWeatherDataByCity(city, "d3e76d9f4759e01c064d1b34ba9ff19e",
-                "metric")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(weatherData -> showWeatherMarker(weatherData));
+    private fun receiveWeather(city: String) {
+        mApi.getWeatherDataByCity(
+            city, "d3e76d9f4759e01c064d1b34ba9ff19e",
+            "metric"
+        )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { weatherData: WeatherData -> showWeatherMarker(weatherData) }
     }
 
-    private void showWeatherMarker(WeatherData weatherData) {
-        LatLng latLng = new LatLng(weatherData.getCoord().getLat(), weatherData.getCoord().getLon());
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Temp: "
-                + weatherData.getMain().getTemp())).showInfoWindow();
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+    private fun showWeatherMarker(weatherData: WeatherData) {
+        val latLng = LatLng(weatherData.coord!!.lat.toDouble(), weatherData.coord!!.lon.toDouble())
+        mMap!!.addMarker(
+            MarkerOptions().position(latLng).title(
+                "Temp: "
+                        + weatherData.main!!.temp
+            )
+        ).showInfoWindow()
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_weather, menu);
-
-        return true;
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_weather, menu)
+        return true
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
+    override fun onPointerCaptureChanged(hasCapture: Boolean) {}
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
         if (id == R.id.action_weather) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         } else if (id == R.id.action_settings) {
 //
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item)
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 
-    @Override
-    public void onClick(View v) {
-        receiveWeather(((Button)v).getText().toString());
+    override fun onClick(v: View) {
+        receiveWeather((v as Button).text.toString())
     }
 }
--- app/src/main/java/com/sv/newsapp/activities/WeatherActivity.kt
+++ app/src/main/java/com/sv/newsapp/activities/WeatherActivity.kt
@@ -1,54 +1,48 @@
-package com.sv.newsapp.activities;
+package com.sv.newsapp.activities
 
-import android.os.Bundle;
-import android.text.InputType;
-import android.view.MenuItem;
-import android.widget.EditText;
+import android.content.DialogInterface
+import android.os.Bundle
+import android.text.InputType
+import android.view.MenuItem
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.sv.newsapp.R
+import com.sv.newsapp.fragments.WeatherFragment
+import com.sv.newsapp.models.CityPreference
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.sv.newsapp.R;
-import com.sv.newsapp.fragments.WeatherFragment;
-import com.sv.newsapp.models.CityPreference;
-
-public class WeatherActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
-
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new WeatherFragment())
-                    .commit();
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, WeatherFragment())
+                .commit()
         }
-
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.change_city){
-            showInputDialog();
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.change_city) {
+            showInputDialog()
         }
-        return false;
+        return false
     }
 
-    private void showInputDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Change city");
-        final EditText input = new EditText(this);
-        input.setInputType(InputType.TYPE_CLASS_TEXT);
-        builder.setView(input);
-        builder.setPositiveButton("Go", (dialog, which) -> changeCity(input.getText().toString()));
-        builder.show();
+    private fun showInputDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Change city")
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+        builder.setPositiveButton("Go") { dialog: DialogInterface?, which: Int -> changeCity(input.text.toString()) }
+        builder.show()
     }
 
-    public void changeCity(String city){
-        WeatherFragment wf = (WeatherFragment)getSupportFragmentManager()
-                .findFragmentById(R.id.container);
-        wf.changeCity(city);
-        new CityPreference(this).setCity(city);
+    fun changeCity(city: String?) {
+        val wf = supportFragmentManager
+            .findFragmentById(R.id.container) as WeatherFragment?
+        wf!!.changeCity(city)
+        CityPreference(this).city = city
     }
 }
